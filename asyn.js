9c2997d508c8767c0995fcf748e04cafda4a5a13
shobhana <shobhanakolukonda@gmail.com>
2024-10-25 11:04:20 +0530

    javascript code completed

---
+++ asyn.js
@@ -0,0 +1,5 @@
+console.log("first");
+setTimeout(()=>{
+console.log("second");
+},9000)
+console.log("third");
---
+++ function.js
@@ -0,0 +1,10 @@
+function sayHello()
+{
+    console.log("this is sayHello function");
+}
+function sayhii()
+{
+    console.log("this is sayhii function");
+}
+sayhii();
+sayHello();
